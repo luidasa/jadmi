0413691a3ab584e0ae84144b1968bce347383641
Luis David Salazar <luidasa@gmail.com>
2022-08-28 16:47:30 -0500

    Se actualizan los usuarios adminsitradores.

---
+++ src/main/java/mx/admino/ConfigMvc.java
@@ -0,0 +1,16 @@
+package mx.admino;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ConfigMvc implements WebMvcConfigurer {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+	    registry.addViewController("/").setViewName("home");
+	    registry.addViewController("/login").setViewName("auth/login");
+	}
+}
---
+++ src/main/java/mx/admino/security/CustomizeAuthenticationSuccessHandler.java
@@ -0,0 +1,31 @@
+package mx.admino.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomizeAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request,
+	        HttpServletResponse response, Authentication authentication)
+	        throws IOException, ServletException {
+	    //set our response to OK status
+	    response.setStatus(HttpServletResponse.SC_OK);
+	    System.out.print(authentication.getName());
+
+	    for (GrantedAuthority auth : authentication.getAuthorities()) {
+	        if ("ADMIN".equals(auth.getAuthority())) {
+	            response.sendRedirect("/admin/panel");
+	        }
+	    }
+	}
+}
